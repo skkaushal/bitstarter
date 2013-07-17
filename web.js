846794ce37f000974e1c46a00cb00dcfe381b118
Sanjay Kaushal <zntrix@gmail.com>
2013-07-17 05:18:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,14 +3,20 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var appdata = fs.readFile('/home/ubuntu/bitstarter/index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  //response.send('Hello World2!');
-    var buffer = fs.readFileSync('index.html');
-    response.send(buffer.toString('utf-8'));
+  response.send(appdata);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+}); 
+ 
 
