dd26df0123a293be701e97dd2b13e3aa0dfa6673
Sanjay Kaushal <zntrix@gmail.com>
2013-07-17 03:10:44 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+  response.send(fs.readFile('/home/ubuntu/bitstarter/index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data));
 });
 
 var port = process.env.PORT || 5000;
